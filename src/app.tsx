f0a7d4838b59787ccd5594ba79283eae21a06929
Decctor <lucasfernandes1101@hotmail.com>
2023-12-19 14:02:52 -0300

    new build with shadow dom

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,18 @@ import "./app.css";
 
 export function App() {
   return (
-    <div
-      className={
-        "w-[450px] h-[600px] flex flex-col border border-300 fixed bottom-0 right-20"
-      }
-    >
-      <h1 className={"font-bold w-full text-center"}>CHAT</h1>
+    <div className="flex min-h-[100vh] w-screen max-w-full flex-col  bg-[#fff] xl:min-h-[100vh]">
+      <div className="flex min-h-[100%] grow ">
+        <div className="flex w-full grow flex-col">
+          <div
+            className={
+              "w-[450px] h-[600px] flex flex-col border border-300 fixed bottom-0 right-20"
+            }
+          >
+            <h1 className={"font-bold w-full text-center"}>CHAT</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from "preact";
+import { App } from "./app.tsx";
 
-render(<App />, document.getElementById('app')!)
+const container = document.createElement("div");
+container.id = "my-widget-container";
+
+const shadowRoot = container.attachShadow({ mode: "open" });
+
+render(<App />, shadowRoot);
+document.body.appendChild(container);
