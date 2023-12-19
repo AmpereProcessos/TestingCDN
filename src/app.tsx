2adc2667458331ab18be55037bd15bb28c7c5189
Decctor <lucasfernandes1101@hotmail.com>
2023-12-19 13:31:02 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import "./app.css";
+
+export function App() {
+  return (
+    <div
+      className={
+        "w-[450px] h-[600px] flex flex-col border border-300 fixed bottom-0 right-20"
+      }
+    >
+      <h1 className={"font-bold w-full text-center"}>CHAT</h1>
+    </div>
+  );
+}
